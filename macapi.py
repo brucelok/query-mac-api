d2bb4bf8a27b0e7b44eeea7612e8aa9b3eb820aa
Bruce Lok <brlok@cisco.com>
2019-09-04 11:46:24 +0800

    add method to handle 404 error

--- macapi.py
+++ macapi.py
@@ -2,7 +2,7 @@
 import sys
 import json
 import urllib2
-from urllib2 import Request, urlopen, URLError
+from urllib2 import Request, urlopen, URLError, HTTPError
 
 '''
 desc: a simple script that query the MAC address's vendor via API call
@@ -14,20 +14,33 @@ return:
     the Company Name associated with that MAC address
 '''
 
-apikey = sys.argv[1]
-macaddress = sys.argv[2]
-url = 'https://api.macaddress.io/v1?apiKey=' + apikey + '&output=json&search=' + macaddress
-# print(url)
+def handle_404_error(code, reason):
+    print 'Failed to reach server, error: ', code
+    print 'Reason: ', reason
+    return
 
-response = urllib2.Request(url)
+def main():
+    apikey = sys.argv[1]
+    macaddress = sys.argv[2]
+    url = 'https://api.macaddress.io/v1?apiKey=' + apikey + '&output=json&search=' + macaddress
+    #url = 'http://www.python.org/f'
+    response = urllib2.Request(url)
 
-try:
-    handler = urllib2.urlopen(response)
-    json_data = json.load(handler)
-except URLError, e:
-    print e.code
-    sys.exit(2)
+    try:
+        handler = urllib2.urlopen(response)
+        json_data = json.load(handler)
+    except HTTPError, e:
+        print e
+        if e.code == 404:
+            handle_404_error(e.code, e.reason)
+            sys.exit(2)
+    except URLError as e:
+        print e
+        sys.exit(2)
 
-searchmac = json_data['macAddressDetails']['searchTerm']
-companyname = json_data['vendorDetails']['companyName']
-print(companyname)
+    searchmac = json_data['macAddressDetails']['searchTerm']
+    companyname = json_data['vendorDetails']['companyName']
+    print(companyname)
+
+if __name__ == '__main__':
+    main()
